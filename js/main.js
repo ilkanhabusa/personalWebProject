86c741647cac83a39034d3f837ac37f5c19da962
ilkanhabusa <122947619+ilkanhabusa@users.noreply.github.com>
2023-05-11 18:23:24 -0400

    Adding a SMTP JS

--- js/main.js
+++ js/main.js
@@ -4,16 +4,14 @@ function sendEmail() {
     let message = document.getElementById("idMessage").value;
     let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Message : ${message} <br>`;
 
-   alert(finalmessage);    /*
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "You were contacted by:"
+   //alert(finalmessage);  
+   Email.send({  
+        SecureToken : "c7be8369-488c-475a-892e-0e8264b24a84",
+        To : 'famherrod.sat@gmail.com',
+        From : "famherrod.sat@gmail.com",
+        Subject : "New email from your personal web site",
+        Body : finalmessage
     }).then(
     message => alert(message)
-    );*/
+    );
 }
